f7b34c3a5a33c6966acddcaac12b2984101dbc1a
Magneto06@mail.ru <magneto06@mail.ru>
2026-05-05 15:43:18 +0600

    Add music playback management to main game class

--- zombie-td/core/src/main/java/com/zombietd/ZombieTD.java
+++ zombie-td/core/src/main/java/com/zombietd/ZombieTD.java
@@ -1,19 +1,33 @@
 package com.zombietd;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ZombieTD extends Game {
     public SpriteBatch batch;
+    Music currentMusic;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        setScreen(new GameScreen(this));
+        setScreen(new MenuScreen(this));
+    }
+
+    public void playMusic(String filename) {
+        if (currentMusic != null) {
+            currentMusic.stop();
+            currentMusic.dispose();
+        }
+        currentMusic = Gdx.audio.newMusic(Gdx.files.internal(filename));
+        currentMusic.setLooping(true);
+        currentMusic.play();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
+        if (currentMusic != null) currentMusic.dispose();
     }
 }
