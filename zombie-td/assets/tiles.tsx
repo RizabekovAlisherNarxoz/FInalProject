5c198e69b124ef3019fe1552d6bde29842d5476e
Magneto06@mail.ru <magneto06@mail.ru>
2026-05-07 14:29:17 +0600

    Add main dungeon tileset definition

---
+++ zombie-td/assets/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="tiles" tilewidth="32" tileheight="32" tilecount="300" columns="15">
+ <image source="underwater-diving-files/PNG/environment/tiles.png" width="480" height="656"/>
+</tileset>
