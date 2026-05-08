8e2a5ad3f8b9a59d8f64f2c4bbc580a07bb2ace5
Magneto06@mail.ru <magneto06@mail.ru>
2026-05-08 13:58:09 +0600

    Add bubbles tileset for underwater effects

---
+++ zombie-td/assets/bubbles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="bubbles" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="underwater-diving-files/PNG/FX/bubbles.png" width="92" height="40"/>
+</tileset>
