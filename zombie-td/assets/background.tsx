6ed2e5e3b7e983ab47fdf410c8ddeeeb68c9b80d
zhadritto <zhadyra.kurmanova@narxoz.kz>
2026-05-08 11:41:27 +0600

    Add background tileset for level scenery

---
+++ zombie-td/assets/background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="background" tilewidth="32" tileheight="32" tilecount="72" columns="9">
+ <image source="underwater-diving-files/PNG/environment/background.png" width="288" height="256"/>
+</tileset>
