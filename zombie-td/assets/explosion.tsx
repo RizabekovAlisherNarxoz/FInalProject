2fb2c49d303c9c7ab6b71e8bde275b5ee438a99d
zhadritto <zhadyra.kurmanova@narxoz.kz>
2026-05-08 15:23:36 +0600

    Add explosion tileset for particle effects

---
+++ zombie-td/assets/explosion.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="explosion" tilewidth="32" tileheight="32" tilecount="40" columns="20">
+ <image source="underwater-diving-files/PNG/FX/explosion.png" width="660" height="82"/>
+</tileset>
